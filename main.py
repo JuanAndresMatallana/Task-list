d9f799404e12aa71b920c43b05a5c2983205a691
andres <j.andres.matallana@gmail.com>
2025-08-21 22:28:12 -0500

    es un programa para tomar notas, importante en este paso solo se abre el archivo json se cargan las tareas, en la funcion principal se establece para que sirve cada uno de los blockes if pero no se ha implementado ninguno

---
+++ main.py
@@ -0,0 +1,61 @@
+import json
+
+import os
+
+
+ARCHIVO_TAREAS = "tareas.json"
+
+def cargar_tareas():
+    if os.path.exits(ARCHIVO_TAREAS):
+        return []
+    with open(ARCHIVO_TAREAS, "r") as f:
+        return json.load(f)
+    
+
+def guardar_tareas(tareas):
+    with open(ARCHIVO_TAREAS, "w") as f:
+        json.dump(tareas, f, indent=4)
+
+
+
+
+def mostrar_menu():
+    print("\n--- Gestor de Tareas ---")
+    print("1. Agregar tarea")
+    print("2. Listar tareas")
+    print("3. Completar tarea")
+    print("4. Salir")
+
+
+
+def main():
+    tareas = cargar_tareas()
+
+    while True:
+        mostrar_menu()
+        opcion = input("Elige una opcion: ")
+        if opcion == "1":
+            descripcion = input("Descripcion de la tarea: ")
+            #Se crea un diccionario para almacenar la tarea
+            nueva_tarea = {
+                "id": len(tareas)+1,
+                "descripcion": descripcion,
+                "completada": False
+
+            }
+            tareas.append(nueva_tarea)
+            guardar_tareas(tareas)
+
+            print("Tarea agregada")
+
+            
+        if opcion == "2":
+            print("Listar las tareas")
+        if opcion == "3":
+            print("Completar las tareas")
+        if opcion == "4":
+            print("Saliendo del programa")
+            break
+        else:
+            print("Opcion invalida intenta de nuevo")
+
